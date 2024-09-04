40300df28b1bcda41f3bb8003251606d52793128
Kwanele Gamedze <kwanelegamedze4@gmail.com>
2024-09-04 23:46:21 +0200

    -fix path 4

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,15 +1,4 @@
 #!/usr/bin/env bash
 echo "Running composer"
-composer global require hirak/prestissimo
 composer install
-npm install
-npm run build
 
-echo "Caching config..."
-php artisan config:cache
-
-echo "Caching routes..."
-php artisan route:cache
-
-echo "Running migrations..."
-php artisan migrate --force
